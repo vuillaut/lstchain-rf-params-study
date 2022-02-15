fc1332f1802db5d0580e067e22375c54104b0a8a
vuillaut <vuillaume@lapp.in2p3.fr>
2022-02-15 17:05:54 +0000

    fix issues

--- grid_job_configs.py
+++ grid_job_configs.py
@@ -18,7 +18,6 @@ results_dir = Path(__file__).parent.joinpath('results').absolute().as_posix()
 def outdir_from_config(config, base_outdir = results_dir):
     
     outdir = os.path.join(base_outdir, os.path.basename(config).split('.')[0])
-    os.makedirs(outdir, exist_ok=True)
 
     return outdir
 
@@ -30,31 +29,34 @@ def main():
     os.makedirs(jobs_dir, exist_ok=True)
     base_slurm = open('base_slurm.slurm').read()
     
+    dl1_gamma_train = glob(dl1_prod.format(particle='gamma-diffuse') + '/*train*.h5')[0]
+    dl1_proton_train = glob(dl1_prod.format(particle='proton') + '/*train*.h5')[0]
+
+    dl1_gamma_test = glob(dl1_prod.format(particle='gamma') + '/off0.4deg/*test*.h5')[0]
+    dl1_proton_test = glob(dl1_prod.format(particle='proton') + '/*test*.h5')[0]
+    dl1_electron_test = glob(dl1_prod.format(particle='electron') + '/*test*.h5')[0]
+
+    for file in [dl1_gamma_test, dl1_electron_test, dl1_proton_test, dl1_gamma_train, dl1_proton_train]:
+        assert os.path.exists(file)
+    
     
     for config in configs:
 
         config_path = Path(config_grid_dir, config).absolute().as_posix()
-
-        dl1_gamma_train = glob(dl1_prod.format(particle='gamma-diffuse') + '/*train*.h5')[0]
-        dl1_proton_train = glob(dl1_prod.format(particle='proton') + '/*train*.h5')[0]
-        
-        dl1_gamma_test = glob(dl1_prod.format(particle='gamma') + '/off0.4deg/*test*.h5')[0]
-        dl1_proton_test = glob(dl1_prod.format(particle='proton') + '/*test*.h5')[0]
-        dl1_electron_test = glob(dl1_prod.format(particle='electron') + '/*test*.h5')[0]
-        
         
         outdir = outdir_from_config(config)
         
         cmds = '\n\n'
-        cmds += f'lstchain_mc_trainpipe --fg {dl1_gamma_train} --fp {dl1_proton_train} -c {config_path} -o {outdir} \n'
-        cmds += f'lstchain_dl1_to_dl2 -f {dl1_gamma_test}  -o {outdir} -c {config_path}\n\n'
-        cmds += f'lstchain_dl1_to_dl2 -f {dl1_proton_test}  -o {outdir} -c {config_path}\n\n'
-        cmds += f'lstchain_dl1_to_dl2 -f {dl1_electron_test} -o {outdir} -c {config_path}\n\n'
+        cmds += f'lstchain_mc_trainpipe --fg {dl1_gamma_train} --fp {dl1_proton_train} -c {config_path} -o {outdir} \n\n'
         
+        cmds += f'lstchain_dl1_to_dl2 -f {dl1_gamma_test}  -o {outdir} -p {outdir} -c {config_path}\n\n'
+        cmds += f'lstchain_dl1_to_dl2 -f {dl1_proton_test}  -o {outdir} -p {outdir} -c {config_path}\n\n'
+        cmds += f'lstchain_dl1_to_dl2 -f {dl1_electron_test} -o {outdir} -p {outdir} -c {config_path}\n\n'
         
-        dl2_gamma_test = dl1_gamma_test.replace('dl1', 'dl2')
-        dl2_proton_test = dl1_proton_test.replace('dl1', 'dl2')
-        dl2_electron_test = dl1_electron_test.replace('dl1', 'dl2')
+
+        dl2_gamma_test = os.path.join(outdir, os.path.basename(os.path.realpath(dl1_gamma_test)).replace('dl1', 'dl2'))
+        dl2_proton_test = os.path.join(outdir, os.path.basename(os.path.realpath(dl1_proton_test)).replace('dl1', 'dl2'))
+        dl2_electron_test = os.path.join(outdir, os.path.basename(os.path.realpath(dl1_electron_test)).replace('dl1', 'dl2'))
         sens_file = os.path.join(outdir, f"sens_{config.split('.')[0]}.fits.gz")
         
         
@@ -67,6 +69,7 @@ def main():
             job_file.write(base_slurm.format(JOB_NAME=config.split('.')[0]))
             job_file.write(cmds)
                                  
+        run(['sbatch', job_sh])
         
     
 if __name__ == '__main__':
